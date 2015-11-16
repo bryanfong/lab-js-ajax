30c8a80054701da065249d7a312656d6918cc471
Bryan Fong <bryan.fch@gmail.com>
2015-11-16 17:55:58 +0800

    list and create is done

--- js/jquery.app.js
+++ js/jquery.app.js
@@ -1,1 +1,27 @@
-// Your solution here
+$().ready(function(){
+  $.get('https://api.doughnuts.ga/doughnuts')
+    .done(function(doughnuts){
+      doughnuts.forEach(function(elem){
+        //
+        $('#doughnuts').append('<li><b>Flavor:</b>' + elem.flavor +
+          '| <b>Style:</b>' + elem.style + '</li>');
+      })
+    })
+  $('#new-doughnut').on('submit', function(e){
+    e.preventDefault();
+    $.ajax('https://api.doughnuts.ga/doughnuts',
+      {
+        method:'POST',
+        data: {
+          flavor: $('#new-doughnut-flavor').val(),
+          style: $('#new-doughnut-style').val()
+        }
+      })
+    .done(function(data){
+        $('#doughnuts').append('<li><b>Flavor:</b>' + data.flavor +
+          '| <b>Style:</b>' + data.style + '</li>');
+    });
+  })
+
+
+})
